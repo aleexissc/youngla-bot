251b6d4c4cf33e972e6470bb089a088f5463b1bd
aleexissc <4x657hfz5b@privaterelay.appleid.com>
2025-11-27 01:10:37 -0600

    Update bot_youngla.py

--- bot_youngla.py
+++ bot_youngla.py
@@ -148,14 +148,16 @@ session = tls_client.Session(
 )
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/537.36 "
-                  "(KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
-    "Accept": "*/*",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.9",
-    "Referer": "https://www.google.com/",
-    "Connection": "keep-alive"
+    "Referer": "https://www.youngla.com/",
+    "Connection": "keep-alive",
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Dest": "document",
+    "Upgrade-Insecure-Requests": "1"
 }
-
 # ================= FUNCIONES =================
 
 def send_discord_message(message: str) -> None:
